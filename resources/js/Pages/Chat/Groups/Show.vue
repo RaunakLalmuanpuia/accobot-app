a03b416eeda092cb0e6e9ca0090d40cee29f129e
Raunak <rlpuia@outlook.com>
2026-04-28 11:34:02 +0530

    New build

--- resources/js/Pages/Chat/Groups/Show.vue
+++ resources/js/Pages/Chat/Groups/Show.vue
@@ -1,6 +1,7 @@
 <template>
     <AuthenticatedLayout>
         <template #header>
+
             <div class="flex items-center gap-3">
                 <Link :href="route('chat.groups.index', { tenant: tenant.id })"
                     class="text-gray-400 hover:text-gray-600">
